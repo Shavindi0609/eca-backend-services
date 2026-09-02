790574133c904ac85c69047aa1757c1de7b44c07
#Shanu0609@ <shavindialoka69@gmail.com>
2026-09-02 14:46:08 +0530

    Add cloud sql auth proxy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   apps : [
     {
+      name: "cloud-sql-auth-proxy",
+      script: " ./cloud-sql-proxy project-c2d114f1-e0c4-497d-a05:asia-southeast1:postgres-vm --private-ip",
+      log_file: "./logs/cloud-sql-proxy-log"
+    },
+    {
       name   : "user-service",
       script : "java -jar ./user-service/target/user-service-0.0.1-SNAPSHOT.jar",
       log_file: "./logs/user-service.log",
